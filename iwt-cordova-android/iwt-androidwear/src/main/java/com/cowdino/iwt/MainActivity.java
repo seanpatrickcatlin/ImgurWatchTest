91a1a1915032cbf4f98a87c506fcf0dcfce717b2
Sean Catlin <catlin.sean@gmail.com>
2015-08-25 23:06:22 -0400

    improve layout to allow for larger images, setup click listeners

--- iwt-cordova-android/iwt-androidwear/src/main/java/com/cowdino/iwt/MainActivity.java
+++ iwt-cordova-android/iwt-androidwear/src/main/java/com/cowdino/iwt/MainActivity.java
@@ -3,10 +3,11 @@ package com.cowdino.iwt;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
-    //private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +17,56 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                //mTextView = (TextView) stub.findViewById(R.id.text);
+                Button previousButton = (Button)stub.findViewById(R.id.previous_button);
+                Button nextButton = (Button)stub.findViewById(R.id.next_button);
+                Button startButton = (Button)stub.findViewById(R.id.start_button);
+
+                previousButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showPreviousImage();
+                    }
+                });
+
+                nextButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showNextImage();
+                    }
+                });
+
+                startButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Button b = (Button)v;
+                        if(b.getText().toString().equalsIgnoreCase(
+                                v.getContext().getResources().getString(R.string.start))) {
+                            startAutoPlay();
+                            b.setText(R.string.stop);
+                        } else {
+                            stopAutoPlay();
+                            b.setText(R.string.start);
+                        }
+                    }
+                });
             }
         });
+
+    }
+
+    private void stopAutoPlay() {
+        Log.v(getClass().getName(), "stopAutoPlay");
+    }
+
+    private void startAutoPlay() {
+        Log.v(getClass().getName(), "startAutoPlay");
+    }
+
+    private void showNextImage() {
+        Log.v(getClass().getName(), "showNextImage");
+    }
+
+    private void showPreviousImage() {
+        Log.v(getClass().getName(), "showPreviousImage");
     }
 }
